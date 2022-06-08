c5dcbb2cbd1eb2a9a0b254bed1b7cd3144947bf1
ericseti41 <eric.seti41@gmail.com>
2022-06-08 20:37:15 +0700

    Tugas 6 Callback Part 2

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,4 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
+const fs = require("fs");
 
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
@@ -18,7 +19,37 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  let hasilFinal = [];
+  fs.readFile(file1, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      let hasilParse1 = JSON.parse(data);
+      let hasilSplit1 = hasilParse1.message.split(" ");
+      hasilFinal.push(hasilSplit1[1]);
+    }
+  });
+  fs.readFile(file2, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      let hasilParse2 = JSON.parse(data);
+      let hasilSplit2 = hasilParse2[0].message.split(" ");
+      hasilFinal.push(hasilSplit2[1]);
+    }
+  });
+  fs.readFile(file3, (err, data) => {
+    if (err) {
+      return fnCallback(err, null);
+    } else {
+      let hasilParse3 = JSON.parse(data);
+      let hasilSplit3 = hasilParse3[0].data.message.split(" ");
+      hasilFinal.push(hasilSplit3[1]);
+    }
+    fnCallback(null, hasilFinal);
+  });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
